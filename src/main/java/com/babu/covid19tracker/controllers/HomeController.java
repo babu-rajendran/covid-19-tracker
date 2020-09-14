64b03b5d5c6b424167bf3ae9e7b85f786238a1bd
Babu Rajendran <babu.rajendran@sjsu.edu>
2020-09-14 17:45:38 +0530

    Changed lambdas to method references

--- src/main/java/com/babu/covid19tracker/controllers/HomeController.java
+++ src/main/java/com/babu/covid19tracker/controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.babu.covid19tracker.controllers;
 
 import com.babu.covid19tracker.models.LocationStats;
 import com.babu.covid19tracker.services.Covid19DataService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +11,17 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    @Autowired
-    Covid19DataService covid19DataService;
+    final Covid19DataService covid19DataService;
+
+    public HomeController(Covid19DataService covid19DataService) {
+        this.covid19DataService = covid19DataService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
         List<LocationStats> allStats = covid19DataService.getAllStats();
-        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
-        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
